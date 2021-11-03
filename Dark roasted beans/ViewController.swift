01d1deb90c63dd66ca70ddf57dbe8e8987b0c9a4
Luc Daalmeijer <lucd877@gmail.com>
2021-11-03 19:36:49 +0100

    [COF-001] Initial setup

---
+++ Dark roasted beans/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dark roasted beans
+//
+//  Created by Luc Daalmeijer on 03/11/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
